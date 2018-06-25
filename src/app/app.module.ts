0b18c2fa72e26e8253dc1ad1a85930ce3c9dcdad
Kieran Whiteman <kieran@kieran.co>
2018-06-25 13:55:04 +0100

    Add copy ability

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { DecodePipe } from './pipes/decode.pipe';
 import { EncodePipe } from './pipes/encode.pipe';
+import { CopyTextDirective } from './copy-text.directive';
 
 @NgModule({
   declarations: [
     AppComponent,
     DecodePipe,
-    EncodePipe
+    EncodePipe,
+    CopyTextDirective
   ],
   imports: [
     BrowserModule,
